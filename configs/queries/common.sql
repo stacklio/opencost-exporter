76bcbdd2a31b60126213afed4bba357f827701f7
Bart Clijsner <bart.clijsner@pxl.be>
2023-04-06 16:15:43 +0200

    Add resourceId and item description to the query

--- configs/queries/common.sql
+++ configs/queries/common.sql
@@ -1,9 +1,11 @@
 select
     (`bill/BillingPeriodStartDate` || "-" || `bill/BillingPeriodEndDate`)  as `period`,
 
+    `lineItem/ResourceId` as `resourceId`,
     `product/ProductName` as `product`,
     `lineItem/Operation` as `operation`,
     `lineItem/LineItemType` as `item_type`,
+    `lineItem/LineItemDescription` as `item_description`,
 
     `lineItem/UsageType` as `usage_type`,
     `pricing/unit` as `usage_unit`,
@@ -14,12 +16,14 @@ select
 from `report-current.csv`
 where `lineItem/UnblendedCost` > 0
 group by
+    `lineItem/ResourceId`,
     `bill/BillingPeriodStartDate`,
     `bill/BillingPeriodEndDate`,
     `product/ProductName`,
     `lineItem/Operation`,
     `lineItem/LineItemType`,
-
+    `lineItem/LineItemDescription`,
+    
     `lineItem/UsageType`,
     `pricing/unit`,
     `lineItem/CurrencyCode`
